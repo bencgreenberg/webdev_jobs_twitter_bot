6d63ddc04fc1a1f6a11e3288fa2fb59dc7931514
benhayehudi <bengreenberg@gmail.com>
2017-11-05 15:04:29 -0500

    updated with heroku env variables

--- bot.js
+++ bot.js
@@ -1,7 +1,14 @@
 // define the dependencies
 const twit = require('twit');
 
-const Twitter = new twit(process.env.consumer_key, process.env.consumer_secret, process.env.access_token, process.env.access_token_secret);
+const config = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
+}
+
+const Twitter = new twit(config);
 const stream = Twitter.stream('user');
 
 // RETWEETER
